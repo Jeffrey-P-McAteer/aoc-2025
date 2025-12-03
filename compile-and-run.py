8a9dd61f0f764d5b9d8fa8095981a8c6234dde91
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-12-03 17:13:46 -0500

    misc

---
+++ compile-and-run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = [
+#
+# ]
+# ///
+
+import os
+import sys
+import subprocess
+import shutil
+import traceback
+import shlex
+
+REPO_DIR = os.path.dirname(__file__)
+
+def no_err(callback, d_val=None):
+  try:
+    return callback()
+  except:
+    return d_val
+
+def main(args=sys.argv):
+  days_to_run = []
+  for arg in args:
+    for arg_int in arg.split(','):
+      no_err(lambda: days_to_run.append(int(arg_int)))
+
+  for dnum in days_to_run:
+    print(f'= = = = day{dnum} = = = =')
+    directory = os.path.join(REPO_DIR, f'day{dnum}')
+    main_asm = os.path.join(directory, f'day{dnum}.s')
+
+
+
+if __name__ == '__main__':
+  main()
+
